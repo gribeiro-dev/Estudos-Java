9c6ff872d66cdad21544eb12c9f827444e0b1e24
gribeiro-dev <irmaosfgg@gmail.com>
2026-07-14 23:57:44 -0300

    feat: implementacao de funcao de nomes e futura funcao de limpar tela no exercicioNotasProfessor

--- src/estrutura_dados/ExercicioNotasProfessor.java
+++ src/estrutura_dados/ExercicioNotasProfessor.java
@@ -10,11 +10,17 @@ public class ExercicioNotasProfessor {
 
     public static final int MOSTRAR_RELATORIO = 3;
 
-    public static final int SAIR_DO_SISTEMA = 4;
+    public static final int MUDAR_NOME_ALUNO = 4;
+
+    public static final int LIMPAR_TELA = 5;
+
+    public static final int SAIR_DO_SISTEMA = 6;
 
     public static void main(String[] args) throws InterruptedException {
 
         Scanner teclado = new Scanner(System.in);
+
+        String nomeDoAluno = null;
 
         int numeroDeAlunosDaLista = 0;
 
@@ -32,7 +38,7 @@ public class ExercicioNotasProfessor {
 
         int numeroDoAluno = 0;
 
-        char escolhaContinuarOuSairDoSistema = 'N';
+        String escolhaContinuarOuSairDoSistema = "S";
 
         // Iniciamos o codigo aqui, esta parte eh a introducao
 
@@ -50,9 +56,11 @@ public class ExercicioNotasProfessor {
 
         double[] listaDeNotasDosAlunos = new double[numeroDeAlunosDaLista];
 
+        String[] listaDeNomesDosAlunos = new String[numeroDeAlunosDaLista];
+
         // Indo para a parte do codigo responsavel pelo funcionamento do sistema inteiro
 
-        while (escolhaContinuarOuSairDoSistema == 'N') {
+        while (escolhaContinuarOuSairDoSistema.equals("S")) {
 
             System.out.println();
 
@@ -76,7 +84,7 @@ public class ExercicioNotasProfessor {
 
                     }
 
-                    System.out.println("> | Aluno " + (numeroDoAluno) + " (" + listaDeNotasDosAlunos[numeroDoAluno - 1] + ")" + " | Digite a nova nota para o aluno " + numeroDoAluno + ": ");
+                    System.out.print("> | Aluno " + (numeroDoAluno) + " (" + listaDeNotasDosAlunos[numeroDoAluno - 1] + ")" + " | Digite a nova nota para o aluno " + numeroDoAluno + ": ");
 
                     notaNovaParaAluno = teclado.nextDouble();
 
@@ -90,11 +98,32 @@ public class ExercicioNotasProfessor {
 
                     listaDeNotasDosAlunos[numeroDoAluno - 1] += notaNovaParaAluno;
 
-                    System.out.println("A nota nova foi atribuida para a nota atual, retornando ao menu em 3 segundos");
+                    System.out.print(">> A nota nova foi atribuida para a nota atual, deseja voltar? (Digite [S = Voltar ou N = Sair]");
 
-                    Thread.sleep(3000);
+                    escolhaContinuarOuSairDoSistema = teclado.next();
 
-                    break;
+                    while (!escolhaContinuarOuSairDoSistema.equalsIgnoreCase("S") && !escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+                        System.out.println("> | Este valor digitado nao esta valido, tente novamente com S ou N: ");
+                        escolhaContinuarOuSairDoSistema = teclado.next();
+                    }
+
+                    if (escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+
+                        System.out.println("> | Saindo do sistema em 3 segundos...");
+
+                        Thread.sleep(300);
+
+                        break;
+
+                    } else {
+
+                        System.out.println("> | Voltando ao menu em 3 segundos...");
+
+                        Thread.sleep(3000);
+
+                        break;
+
+                    }
 
 
                 case MODIFICAR_NOTA:
@@ -125,15 +154,38 @@ public class ExercicioNotasProfessor {
 
                     listaDeNotasDosAlunos[numeroDoAluno - 1] = notaNovaParaAluno;
 
-                    System.out.println(">> A nota nova foi atribuida ao aluno, para verificar os relatorios, escolha 3 no menu...");
+                    System.out.println(">> A nota nova foi atribuida ao aluno, deseja voltar? (Digite [S = Voltar ou N = Sair]");
 
-                    System.out.println("Retornando ao menu em 3 segundos...");
+                    escolhaContinuarOuSairDoSistema = teclado.next();
 
-                    Thread.sleep(3000);
+                    while (!escolhaContinuarOuSairDoSistema.equalsIgnoreCase("S") && !escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+                        System.out.println("> | Este valor digitado nao esta valido, tente novamente com S ou N: ");
+                        escolhaContinuarOuSairDoSistema = teclado.next();
+                    }
 
-                    break;
+                    if (escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+
+                        System.out.println("> | Saindo do sistema em 3 segundos...");
+
+                        Thread.sleep(300);
+
+                        break;
+
+                    } else {
+
+                        System.out.println("> | Voltando ao menu em 3 segundos...");
+
+                        Thread.sleep(3000);
+
+                        break;
+
+                    }
 
                 case MOSTRAR_RELATORIO:
+
+                    somaDasNotasDosAlunos = 0;
+                    numeroDeAlunosAcimaMed = 0;
+                    numeroDeAlunosAbaixoMed = 0;
 
                     System.out.println("\n===== RELATÓRIO DE NOTAS DOS ALUNOS =====");
                     System.out.println("-----------------------------------------");
@@ -143,12 +195,20 @@ public class ExercicioNotasProfessor {
                     // Loop que percorre toda a lista de notas
                     for (int i = 0; i < listaDeNotasDosAlunos.length; i++) {
                         // Exibe "Aluno 1", "Aluno 2", etc., buscando o índice i (0, 1, 2...)
-                        System.out.printf("Aluno %-6d | %.2f\n", (i + 1), listaDeNotasDosAlunos[i]);
-                        somaDasNotasDosAlunos += listaDeNotasDosAlunos[i];
+
+                        if (listaDeNomesDosAlunos[i] == null) {
+                            System.out.printf("Aluno %-6d | %.2f\n", (i + 1), listaDeNotasDosAlunos[i]);
+                            somaDasNotasDosAlunos += listaDeNotasDosAlunos[i];
+                        } else {
+                            System.out.printf("Aluno: %-15s | %.2f\n", listaDeNomesDosAlunos[i], listaDeNotasDosAlunos[i]);
+                            somaDasNotasDosAlunos += listaDeNotasDosAlunos[i];
+                        }
+
                     }
 
                     mediaDaSalaComAsNotas = somaDasNotasDosAlunos / listaDeNotasDosAlunos.length;
 
+                    // Loop que pega a nota do aluno I, e ve se esta acima da media ou nao
                     for (int i = 0; i < listaDeNotasDosAlunos.length; i++) {
                         if ( listaDeNotasDosAlunos[i] >= mediaDaSalaComAsNotas) {
                             numeroDeAlunosAcimaMed++;
@@ -165,11 +225,121 @@ public class ExercicioNotasProfessor {
 
                     break;
 
+                case MUDAR_NOME_ALUNO:
+
+                    System.out.print(">> Digite o numero do aluno que deseja alterar o nome : ");
+
+                    numeroDoAluno = teclado.nextInt();
+
+                    while (!verificadorDoNumeroEscolhido(numeroDoAluno,listaDeNotasDosAlunos)) {
+
+                        System.out.println(">> Esse numero de estudante esta invalido, tente novamente: ");
+
+                        numeroDoAluno = teclado.nextInt();
+
+                    }
+
+                    teclado.nextLine();
+
+                    System.out.println("> | Nome atual do aluno: " + (listaDeNomesDosAlunos[numeroDoAluno - 1]) + " (" + "Numero: " + numeroDoAluno + ")");
+                    System.out.print("> | Digite o novo nome para o aluno: ");
+
+                    nomeDoAluno = teclado.nextLine();
+
+                    while (!validacaoDeNomeDoAluno(nomeDoAluno, listaDeNomesDosAlunos)) {
+
+                        System.out.println("> | Este nome nao eh valido, utilize somente letras para o nome");
+
+                        System.out.print("> | Digite o novo nome para o aluno: ");
+
+                        nomeDoAluno = teclado.nextLine();
+
+                    }
+
+                    listaDeNomesDosAlunos[numeroDoAluno - 1] = nomeDoAluno;
+
+                    System.out.print("> | O nome ' " + nomeDoAluno + " ' foi adicionado ao aluno. deseja voltar? (Digite [S = Voltar ou N = Sair]: ");
+
+                    escolhaContinuarOuSairDoSistema = teclado.next();
+
+                    while (!escolhaContinuarOuSairDoSistema.equalsIgnoreCase("S") && !escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+                        System.out.println("> | Este valor digitado nao esta valido, tente novamente com S ou N: ");
+                        escolhaContinuarOuSairDoSistema = teclado.next();
+                    }
+
+                    if (escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+
+                        System.out.println("> | Saindo do sistema em 3 segundos...");
+
+                        Thread.sleep(300);
+
+                        break;
+
+                    } else {
+
+                        System.out.println("> | Voltando ao menu em 3 segundos...");
+
+                        Thread.sleep(3000);
+
+                        break;
+
+                    }
+
+
+                case LIMPAR_TELA:
+
+                    System.out.println("---------------------------------------------------");
+
+                    System.out.println(">> Funcao ainda nao foi feita! :) (Proximo commit irei implementar)");
+
+                    System.out.println("---------------------------------------------------");
+
+                    System.out.print("> | Deseja voltar? (Digite [S = Voltar ou N = Sair]: ");
+
+                    escolhaContinuarOuSairDoSistema = teclado.next();
+
+                    while (!escolhaContinuarOuSairDoSistema.equalsIgnoreCase("S") && !escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+                        System.out.println(">> Este valor digitado nao esta valido, tente novamente com S ou N: ");
+                        escolhaContinuarOuSairDoSistema = teclado.next();
+                    }
+
+                    if (escolhaContinuarOuSairDoSistema.equalsIgnoreCase("N")) {
+
+                        System.out.println("> | Saindo do sistema em 3 segundos...");
+
+                        Thread.sleep(300);
+
+                        break;
+
+                    } else {
+
+                        System.out.println("> | Voltando ao menu em 3 segundos...");
+
+                        Thread.sleep(3000);
+
+                        break;
+
+                    }
+
+
                 case SAIR_DO_SISTEMA:
 
-                    escolhaContinuarOuSairDoSistema = 'S';
+                    escolhaContinuarOuSairDoSistema = "N";
 
                     break;
+
+                default:
+
+                    System.out.println();
+
+                    System.out.println("---------------------------------------------------");
+
+                    System.out.println(">> Este valor digitado esta invalido, tente novamente.");
+
+                    System.out.println("---------------------------------------------------");
+
+                    break;
+
 
             }
 
@@ -177,14 +347,20 @@ public class ExercicioNotasProfessor {
     }
 
     public static void exibirMenu () {
+
+        System.out.println("----------------------------------------------------");
         System.out.println("------>> Sistema Escolar para Professores <<--------");
         System.out.println("----------------------------------------------------");
         System.out.println();
         System.out.println(">> (1) - Adicionar notas aos estudantes ");
         System.out.println(">> (2) - Modificar nota do estudante ");
         System.out.println(">> (3) - Mostrar o relatorio dos alunos ");
-        System.out.println(">> (4) - Sair do sistema ");
+        System.out.println(">> (4) - Modificar o nome dos alunos ");
+        System.out.println(">> (5) - Limpar a tela do terminal ");
+        System.out.println(">> (6) - Sair do sistema ");
+        System.out.println();
         System.out.println("---------------------------------------------------");
+        System.out.println();
 
         System.out.print(">> Escolha uma das opções (1-5): ");
 
@@ -224,6 +400,16 @@ public class ExercicioNotasProfessor {
         } else {
             return true;
         }
+
+    }
+
+    public static boolean validacaoDeNomeDoAluno(String nomeDoAluno, String[] listaDeNomesDosAlunos) {
+
+        if (nomeDoAluno == null || nomeDoAluno.trim().isEmpty()) {
+            return false;
+        }
+        return nomeDoAluno.matches("^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$");
+
 
     }
 
